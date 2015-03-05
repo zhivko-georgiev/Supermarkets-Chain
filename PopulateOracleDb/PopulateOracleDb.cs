db1f46c864cf4210c9d12e49561e12c54e830b6a
tr00per92 <zvezdo@mail.bg>
2015-03-05 18:14:28 +0200

    automated oracle db population

---
+++ PopulateOracleDb/PopulateOracleDb.cs
@@ -0,0 +1,26 @@
+﻿namespace PopulateOracleDb
+{
+    using System;
+    using System.IO;
+    using Oracle.ManagedDataAccess.Client;
+
+    public class PopulateOracleDb
+    {
+        public static void Main()
+        {
+            var queries = File.ReadAllText(@"..\..\..\PopulateOracleDb.sql").Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var connection = new OracleConnection(Settings.Default.OracleConnectionString);
+            connection.Open();
+            using (connection)
+            {
+                foreach (var query in queries)
+                {
+                    using (var command = new OracleCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
