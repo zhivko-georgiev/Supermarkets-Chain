954ea13da3b0c5eb5b91853519f7e8ae79b3aeff
Elezar Ivanov <zarkongu@gmail.com>
2015-03-15 15:46:44 +0200

    add ReportsFromZipIntoSQL.cs

---
+++ SupermarketsChain.Helpers/ReportsFromZipIntoSQL.cs
@@ -0,0 +1,22 @@
+﻿namespace SupermarketsChain.Helpers
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using SupermarketsChain.Data;
+    using System.IO;
+    using Ionic.Zip;
+
+    public class ReportsFromZipIntoSQL
+    {
+        public static void Generate()
+        {
+            using (ZipFile zip = ZipFile.Read(@"C:\SoftUni\Database\Teamwork\trunk\Sales-Reports.zip"))
+            {
+                ZipEntry e = zip["Bourgas-Plaza-Sales-Report-20-Jul-2014.xls"];
+                Console.WriteLine("Successfully open file.");
+            }
+        }
+    }
+}
