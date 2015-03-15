98ed67ce31d8abc79412be893d62966f677e5967
Elezar Ivanov <zarkongu@gmail.com>
2015-03-15 14:19:01 +0200

    Successfully open ZIP file and xls document inside

--- SupermarketsChain.ConsoleClient/ConsoleClient.cs
+++ SupermarketsChain.ConsoleClient/ConsoleClient.cs
@@ -9,6 +9,9 @@
     {
         public static void Main()
         {
+            var db = new SupermarketsChainEntities();
+            db.SaveChanges();
+            ReportsFromZipIntoSQL.Generate();
             //OracleDbPopulator.Populate();
             //XmlExpenseDataLoader.AddExpensesToDb();
             //PdfExporter.Export("20-Jul-2014", "22-Jul-2014");
