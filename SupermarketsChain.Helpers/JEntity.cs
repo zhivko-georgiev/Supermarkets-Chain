c13368c64a0c4408aeee27c998723460e97a4dba
Nickolai Panaiotov <nickpanaiotov@gmail.com>
2015-03-16 16:41:35 +0200

    edit usings

--- SupermarketsChain.Helpers/JEntity.cs
+++ SupermarketsChain.Helpers/JEntity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SupermarketsChain.Helpers
+﻿namespace SupermarketsChain.Helpers
 {
     internal class JEntity
     {
