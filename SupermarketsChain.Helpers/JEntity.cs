f72249739285b91154044fe744a54c9a3054653e
Nickolai Panaiotov <nickpanaiotov@gmail.com>
2015-03-16 16:39:11 +0200

    JSON Report in MongoDb
    using MondoDb.Driver

---
+++ SupermarketsChain.Helpers/JEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SupermarketsChain.Helpers
+{
+    internal class JEntity
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string VendorName { get; set; }
+        public decimal QuantitySold { get; set; }
+        public decimal TotalIncomes { get; set; }
+    }
+}
