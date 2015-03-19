8c8afcb03c82416da6ab5f2d48be55f66b040e03
tr00per92 <zvezdo@mail.bg>
2015-03-19 18:52:55 +0200

    ren

--- Helper.Files/Populate-SqLite-Db.sql
+++ Helper.Files/Populate-SqLite-Db.sql
@@ -4,53 +4,53 @@ CREATE TABLE Products (
   TaxPercent INTEGER NOT NULL
 );
 
-INSERT INTO PRODUCTS (Name, TaxPercent)
+INSERT INTO Products (Name, TaxPercent)
 VALUES ('Beer Zagorka', 10);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Chocolate Milka', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Chocolate Milka Special', 8);
-INSERT INTO PRODUCTS (Name, TaxPercent)
+INSERT INTO Products (Name, TaxPercent)
 VALUES ('Croissant 7 days', 25);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Waffle Hiper', 15);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Cake 7 days', 18);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Coca-Cola 0.5l', 10);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Coca-Cola 20l', 10);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Sprite', 10);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Chips Lays', 15);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Chips Pringles', 5);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Yoghurt Vereya', 10);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Milk Vereya 3.6%', 7);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Pasta Casa Bella', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Bread Dobrudza', 16);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Bake Rolls', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ( 'Confiture Bonne Maman', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Jelee Bonne Maman', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Chips Ruffles', 22);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Caffee Lavazza', 20);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Nescafe 3in10', 9);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Nestle LZ', 12);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Nestle Fitness', 13);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Ice Cream Aloma', 14);
-INSERT INTO PRODUCTS (Name, TaxPercent) 
+INSERT INTO Products (Name, TaxPercent) 
 VALUES ('Beer Becks', 21);
