35b71c0ebd2cd627562fd31977a27fe104b09f61
m4r71n85 <martin.tsekov.bg@gmail.com>
2015-03-21 12:47:41 +0200

    same as previous

--- SupermarketsChain.Helpers/MysqlDbManager.cs
+++ SupermarketsChain.Helpers/MysqlDbManager.cs
@@ -6,28 +6,24 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using SupermarketsChain.Data;
 using SupermarketsChain.Models;
+using SupermarketChain.MySql.Data;
 
 namespace SupermarketsChain.Helpers
 {
     public class MysqlDbManager
     {
 
-        private static MySqlConnection mySqlDb;
+        private static SupermarketsChainMySqlEntities mySqlDb;
         private static SupermarketsChainEntities msSqlDb;
         public static void Initialize()
         {
-            //init MySQL connection
-            mySqlDb = new MySqlConnection();
-            var connetionString = "Data Source=localhost;Initial Catalog=supermarketschaindb;User ID=root;Password=";
-            mySqlDb.ConnectionString = connetionString;
-            
-            //init MSSQL connection
+            mySqlDb = new SupermarketsChainMySqlEntities();
             msSqlDb = new SupermarketsChainEntities();
         }
 
         public static void MSSQLToMySql(){
             if(mySqlDb == null){ Initialize(); }
-            
+
             MergeVendors();
 
             Console.ReadLine();
@@ -35,39 +31,14 @@ namespace SupermarketsChain.Helpers
 
         private static void MergeVendors()
         {
-            var mySqlVendors = GetAllVendors();
+            var mySqlVendors = mySqlDb.Vendors.GetAll();
             var msSqlVendors = msSqlDb.Vendors.Where(x => !mySqlVendors.Contains(x.Name)).ToList();
             if (msSqlVendors.Count() > 0)
             {
-                SaveVendors(msSqlVendors);
+                mySqlDb.Vendors.SaveVendors(msSqlVendors);
             }
         }
 
-        private static void SaveVendors(List<Vendor> msSqlVendors)
-        {
-            mySqlDb.Open();
-            var values = string.Join(",", msSqlVendors.Select(x=>"('" + x.Id + "', '" + x.Name + "')").ToList());
-            var query = string.Format("INSERT INTO vendor (id, name) VALUES {0};", values);
-            var command = mySqlDb.CreateCommand();
-            command.CommandText = query;
-            command.ExecuteNonQuery();
-            mySqlDb.Close();
-        }
-
-        private static List<string> GetAllVendors()
-        {
-            mySqlDb.Open();
-            var command = mySqlDb.CreateCommand();
-            var vendors = new List<string>();
-
-            command.CommandText = "SELECT name FROM vendor;";
-            var reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                vendors.Add(reader["name"].ToString());
-            }
-            mySqlDb.Close();
-            return vendors;
-        }
+        
     }
 }
